39e6c8673ce08bdb94b04c786e9f38d13aa34cd6
hminsky <henry.minsky@gmail.com>
2025-05-16 16:57:06 -0400

    add run script

---
+++ run_icebear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Go to the project directory (replace with your absolute path)
+cd ~/src/snowbearGenerator
+
+# Activate virtual environment
+source venv/bin/activate
+set -a source .env
+
+# Run the script
+python icebear.py
+
+# Deactivate virtual environment
+deactivate
